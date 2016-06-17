697b89c7855e3117f432274258e6c377617592fb
Stefan <steftonia@gmail.com>
2016-06-17 13:35:03 +0200

    command was hardcoded

--- handler/sendingRequests.py
+++ handler/sendingRequests.py
@@ -6,7 +6,9 @@ from send import sending
 s = sending()
 if (sys.argv[1] == '1'):
     command = {'command': '1'}
+    print(json.dumps(command))
     s.send('localhost', 'Sesame', 'Sesame', json.dumps(command))
 if (sys.argv[1] == '2'):
-    command =  {'command': '1','itemid':sys.argv[2] }
+    command =  {'command': '2','itemid':sys.argv[2] }
+    print(json.dumps(command))
     s.send('localhost', 'Sesame', 'Sesame', json.dumps(command))
