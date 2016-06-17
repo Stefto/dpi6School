735ab00a21e4a148ec4fe8ceb991ca1b97178357
Stefan <steftonia@gmail.com>
2016-06-17 05:11:33 +0200

    fucking json acts difficult

--- handler/sendingRequests.py
+++ handler/sendingRequests.py
@@ -4,9 +4,9 @@ from send import sending
 
 
 s = sending()
-if (sys.argv[0] == '1'):
-    command = '{"command": "1"}'
+if (sys.argv[1] == '1'):
+    command = {'command': '1'}
     s.send('localhost', 'Sesame', 'Sesame', json.dumps(command))
-if (sys.argv[0] == '2'):
-    command = '{"command": "1","itemid":' + sys.argv[1] + ' }'
+if (sys.argv[1] == '2'):
+    command =  {'command': '1','itemid':sys.argv[2] }
     s.send('localhost', 'Sesame', 'Sesame', json.dumps(command))
